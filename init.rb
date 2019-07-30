a1dea0e4415a5c96d03d81bde1615742371c91bf
SBriand <stephane.briand@seasidetech.net>
2019-07-30 08:45:15 +0200

    Include update from original author. Based on original version 0.0.3

--- init.rb
+++ init.rb
@@ -1,40 +1,41 @@
 Redmine::Plugin.register :redmine_include_macro_extension do
- name 'Include macro extension plugin'
- author 'Stephane Briand'
- description 'This plugin makes possible include wiki section. And it also provide icons to directly view or edit included wiki page. Original author is Taiki I.'
- version '0.0.3'
- url 'https://github.com/sbriand/redmine_include_macro_extension'
- author_url 'https://github.com/taikii'
- 
- Redmine::WikiFormatting::Macros.register do
-    desc "Includes a wiki page. Examples:\n\n" +
-           "{{include(Foo)}}\n" +
-           "{{include(Foo, Bar)}} -- to include Bar section of Foo page\n" +
-           "{{include(projectname:Foo)}} -- to include a page of a specific project wiki"
-    macro :include do |obj, args|
-      out = ''
-      page = Wiki.find_page(args.first.to_s, :project => @project)
-      raise 'Page not found' if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
-      @included_wiki_pages ||= []
+  name 'Include macro extension plugin'
+  author 'Taiki IKEGAME'
+  description 'This plugin makes possible include wiki section.'
+  version '0.0.4'
+  url 'https://github.com/sbriand/redmine_include_macro_extension'
+  author_url 'https://github.com/taikii'
 
+  Redmine::WikiFormatting::Macros.register do
+
+    Redmine::WikiFormatting::Macros::Definitions.send :alias_method, "macro_include_original", "macro_include"
+
+    desc "Includes a wiki page. Examples:\n\n" +
+        "{{include(Foo)}}\n" +
+        "{{include(Foo, Bar)}} -- to include Bar section of Foo page\n" +
+        "{{include(projectname:Foo)}} -- to include a page of a specific project wiki"
+    macro :include do |obj, args|
       if args.size == 1
-        raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title)
-        @included_wiki_pages << page.title
-        out = textilizable(page.content, :text, :attachments => page.attachments, :headings => false)
-        @included_wiki_pages.pop
+        args.push("DUMMY")
+        send("macro_include_original", obj, args)
       else
+        page = Wiki.find_page(args.first.to_s, :project => @project)
+        raise 'Page not found' if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+        @included_wiki_pages ||= []
+
         secname = args[1].to_s
-        index = 0
+
+        regex = nil
         case Setting.text_formatting
         when "textile"
-          page.content.text.scan(/(?:\A|\r?\n\s*\r?\n)h\d+\.[ \t]+(.*?)(?:\r?\n\s*\r?\n|\z)/m).each.with_index(1) do |matched, i|
-            if matched.first.gsub(/[\r\n]/, '') == secname
-              index = i
-              break
-            end
-          end
+          regex = '(?:\A|\r?\n\s*\r?\n)h\d+\.[ \t]+(.*?)(?=\r?\n\s*\r?\n|\z)'
         when "markdown"
-          page.content.text.scan(/(?:\A|\r?\n)#+ +(.*?)(?:\r?\n|\z)/).each.with_index(1) do |matched, i|
+          regex = '(?:\A|\r?\n)#+ +(.*?)(?=\r?\n|\z)'
+        end
+
+        index = 0
+        if regex
+          page.content.text.scan(/#{regex}/m).each.with_index(1) do |matched, i|
             if matched.first.gsub(/[\r\n]/, '') == secname
               index = i
               break
@@ -47,27 +48,39 @@ Redmine::Plugin.register :redmine_include_macro_extension do
           sectext, hash = Redmine::WikiFormatting.formatter.new(page.content.text).get_section(index)
         end
 
-        if sectext
-          raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title) || @included_wiki_pages.include?(page.title + ':' + secname)
-          @included_wiki_pages << page.title + ':' + secname
-          out = textilizable(sectext, :attachments => page.attachments, :headings => false)
-          @included_wiki_pages.pop
-        end
-      end
+        raise 'Section not found' if sectext.nil?
+        raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title) || @included_wiki_pages.include?(page.title + ':' + secname)
 
-      content_tag('div',
-                  link_to(l(:button_view), project_wiki_page_path(page.project, page.title),
+        if args.size > 2
+          options = args[2..-1]
+
+          if options.include?("nosubsection")
+            subsecidx = sectext.index(/#{regex}/m, sectext.index(/#{regex}/m) + 1)
+            if subsecidx
+              sectext = sectext[0 .. (subsecidx - 1)]
+            end
+          end
+          if options.include?("noheading")
+            sectext.sub!(/#{regex}/m, '')
+          end
+        end
+
+        @included_wiki_pages << page.title + ':' + secname
+        out = textilizable(sectext, :attachments => page.attachments, :headings => false)
+        @included_wiki_pages.pop
+        content_tag('div',
+                  link_to(l(:label_button_view), project_wiki_page_path(page.project, page.title),
                           :class => 'icon-only icon-magnifier'),
                   :class => "contextual",
-                  :title => l(:button_view),
+                  :title => l(:label_button_view),
                   :id => "view-#{page.title}") +
       content_tag('div',
-                  link_to(l(:button_edit), project_wiki_page_path(page.project, page.title) + '/edit',
+                  link_to(l(:label_button_edit), project_wiki_page_path(page.project, page.title) + '/edit',
                           :class => 'icon-only icon-edit'),
                   :class => "contextual",
-                  :title => l(:button_edit),
-                  :id => "edit-#{page.title}") + out.html_safe
+                  :title => l(:label_button_edit),
+                  :id => "edit-#{page.title}") + out
+      end
     end
- end
+  end
 end
-
